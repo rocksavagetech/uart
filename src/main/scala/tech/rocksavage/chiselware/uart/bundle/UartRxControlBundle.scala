2a96954c693ecefcf9746501992259a72f540ae8
Nick Allison <nickrallison1@gmail.com>
2025-02-12 07:40:14 -0700

    starting parity

---
+++ src/main/scala/tech/rocksavage/chiselware/uart/bundle/UartRxControlBundle.scala
@@ -0,0 +1,17 @@
+// This code is licensed under the Apache Software License 2.0 (see LICENSE.MD)
+
+package tech.rocksavage.chiselware.uart.bundle
+
+import chisel3._
+import chisel3.util._
+import tech.rocksavage.chiselware.uart.param.UartParams
+
+// from the perspective of the UART which is receiving data
+class UartControlBundle(params: UartParams) extends Bundle {
+
+    // configuration inputs
+    val clocksPerBitDb  = Input(UInt((log2Ceil(params.maxClocksPerBit) + 1).W))
+    val numOutputBitsDb = Input(UInt((log2Ceil(params.maxOutputBits) + 1).W))
+    val useParityDb     = Input(Bool())
+
+}
