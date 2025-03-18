63f6eee9ce7767518daf92a41fe59b697b630946
Nick Allison <nickrallison1@gmail.com>
2025-03-17 21:33:02 -0600

    update configs

--- src/main/scala/tech/rocksavage/chiselware/uart/UartConfig.scala
+++ src/main/scala/tech/rocksavage/chiselware/uart/UartConfig.scala
@@ -5,54 +5,54 @@ import tech.rocksavage.traits.ModuleConfig
 
 class UartConfig extends ModuleConfig {
     override def getDefaultConfigs: Map[String, Any] = Map(
-//      "9bits_2sync_900000baud" -> Seq(
-//        UartParams(
-//          addressWidth = 32,
-//          dataWidth = 32,
-//          wordWidth = 8,
-//          maxOutputBits = 9,
-//          syncDepth = 2,
-//          maxBaudRate = 921_600,
-//          maxClockFrequency = 1_000_000
-//        ),
-//        false
-//      ),
-//      "8bits_2sync_900000baud" -> Seq(
-//        UartParams(
-//          addressWidth = 32,
-//          dataWidth = 32,
-//          wordWidth = 8,
-//          maxOutputBits = 8,
-//          syncDepth = 2,
-//          maxBaudRate = 921_600,
-//          maxClockFrequency = 1_000_000
-//        ),
-//        false
-//      ),
-//      "9bits_32sync_900000baud" -> Seq(
-//        UartParams(
-//          addressWidth = 32,
-//          dataWidth = 32,
-//          wordWidth = 8,
-//          maxOutputBits = 9,
-//          syncDepth = 32,
-//          maxBaudRate = 921_600,
-//          maxClockFrequency = 1_000_000
-//        ),
-//        false
-//      ),
-//      "9bits_2sync_115000baud" -> Seq(
-//        UartParams(
-//          addressWidth = 32,
-//          dataWidth = 32,
-//          wordWidth = 8,
-//          maxOutputBits = 9,
-//          syncDepth = 2,
-//          maxBaudRate = 115200,
-//          maxClockFrequency = 1_000_000
-//        ),
-//        false
-//      ),
+      "9bits_2sync_900000baud" -> Seq(
+        UartParams(
+          addressWidth = 32,
+          dataWidth = 32,
+          wordWidth = 8,
+          maxOutputBits = 9,
+          syncDepth = 2,
+          maxBaudRate = 921_600,
+          maxClockFrequency = 1_000_000
+        ),
+        false
+      ),
+      "8bits_2sync_900000baud" -> Seq(
+        UartParams(
+          addressWidth = 32,
+          dataWidth = 32,
+          wordWidth = 8,
+          maxOutputBits = 8,
+          syncDepth = 2,
+          maxBaudRate = 921_600,
+          maxClockFrequency = 1_000_000
+        ),
+        false
+      ),
+      "9bits_32sync_900000baud" -> Seq(
+        UartParams(
+          addressWidth = 32,
+          dataWidth = 32,
+          wordWidth = 8,
+          maxOutputBits = 9,
+          syncDepth = 32,
+          maxBaudRate = 921_600,
+          maxClockFrequency = 1_000_000
+        ),
+        false
+      ),
+      "9bits_2sync_115000baud" -> Seq(
+        UartParams(
+          addressWidth = 32,
+          dataWidth = 32,
+          wordWidth = 8,
+          maxOutputBits = 9,
+          syncDepth = 2,
+          maxBaudRate = 115200,
+          maxClockFrequency = 1_000_000
+        ),
+        false
+      ),
       "8bits_2sync_115000baud" -> Seq(
         UartParams(
           addressWidth = 32,
