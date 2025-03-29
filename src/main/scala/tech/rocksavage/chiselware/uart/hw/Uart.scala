6688d4a7b6e393bcd83f1535b873dc777592b483
Nick Allison <nickrallison1@gmail.com>
2025-03-28 18:49:37 -0600

    adding verbose guards

--- src/main/scala/tech/rocksavage/chiselware/uart/hw/Uart.scala
+++ src/main/scala/tech/rocksavage/chiselware/uart/hw/Uart.scala
@@ -520,33 +520,35 @@ class Uart(val uartParams: UartParams, formal: Boolean) extends Module {
       error.rxError =/= UartRxError.None || error.txError =/= UartTxError.None || error.topError =/= UartTopError.None || error.addrDecodeError =/= AddrDecodeError.None
     ) {
         // Print debug message
-        printf(
-          "[Uart.scala DEBUG] Error detected Bits: %b\n",
-          error.asUInt
-        )
-        when(error.rxError =/= UartRxError.None) {
+        if (uartParams.verbose) {
             printf(
-              "[Uart.scala DEBUG] RX Error detected: %b\n",
-              error.rxError.asUInt
+              "[Uart.scala DEBUG] Error detected Bits: %b\n",
+              error.asUInt
             )
-        }
-        when(error.txError =/= UartTxError.None) {
-            printf(
-              "[Uart.scala DEBUG] TX Error detected: %b\n",
-              error.txError.asUInt
-            )
-        }
-        when(error.topError =/= UartTopError.None) {
-            printf(
-              "[Uart.scala DEBUG] Top Error detected: %b\n",
-              error.topError.asUInt
-            )
-        }
-        when(error.addrDecodeError =/= AddrDecodeError.None) {
-            printf(
-              "[Uart.scala DEBUG] AddrDecode Error detected: %b\n",
-              error.addrDecodeError.asUInt
-            )
+            when(error.rxError =/= UartRxError.None) {
+                printf(
+                  "[Uart.scala DEBUG] RX Error detected: %b\n",
+                  error.rxError.asUInt
+                )
+            }
+            when(error.txError =/= UartTxError.None) {
+                printf(
+                  "[Uart.scala DEBUG] TX Error detected: %b\n",
+                  error.txError.asUInt
+                )
+            }
+            when(error.topError =/= UartTopError.None) {
+                printf(
+                  "[Uart.scala DEBUG] Top Error detected: %b\n",
+                  error.topError.asUInt
+                )
+            }
+            when(error.addrDecodeError =/= AddrDecodeError.None) {
+                printf(
+                  "[Uart.scala DEBUG] AddrDecode Error detected: %b\n",
+                  error.addrDecodeError.asUInt
+                )
+            }
         }
 
     }
@@ -568,7 +570,9 @@ class Uart(val uartParams: UartParams, formal: Boolean) extends Module {
         error.topError := UartTopError.None
 
         // Print debug message
-        printf("[Uart.scala DEBUG] Clearing error registers\n")
+        if (uartParams.verbose) {
+            printf("[Uart.scala DEBUG] Clearing error registers\n")
+        }
     }
 
     when(rxFlush) {
